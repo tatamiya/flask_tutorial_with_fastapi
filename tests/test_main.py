6c1ba4665d283c03464614fb99c4c6d9adbba6de
tatamiya <tamyuji1002@gmail.com>
2021-03-27 13:00:35 +0900

    hello world

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from fastapir.main import app
+
+client = TestClient(app)
+
+
+def test_hello():
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.json() == "Hello, World!"
