2daf97a59a170a3ecc5ca1dcab7d10c0f8ce98d0
tatamiya <tamyuji1002@gmail.com>
2021-03-27 14:28:08 +0900

    index page

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,12 @@ def test_hello():
     response = client.get("/hello")
     assert response.status_code == 200
     assert response.json() == "Hello, World!"
+
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"FastAPIr" in response.content
+    assert b"Register" in response.content
+    assert b"Log In" in response.content
+    assert b"Posts" in response.content
