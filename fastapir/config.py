bae9cba5a73b9fe58e3734f2e5758285ccc7f5a2
tatamiya <tamyuji1002@gmail.com>
2021-04-29 19:59:10 +0900

    change: connect to Cloud SQL

--- fastapir/config.py
+++ fastapir/config.py
@@ -3,6 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     session_secret_key: str
+    database_url: str = "sqlite:///./fastapir.db"
 
     class Config:
         env_file = ".env"
--- fastapir/db/database.py
+++ fastapir/db/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapir.db"
+from fastapir.config import Settings
+
+SQLALCHEMY_DATABASE_URL = Settings().database_url
 
 Base = declarative_base()
 
