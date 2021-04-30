c6250a4264ee718729768bb5f5ecde043a61c144
tatamiya <tamyuji1002@gmail.com>
2021-04-30 11:50:38 +0900

    fix connect_args in db connection

--- fastapir/db/database.py
+++ fastapir/db/database.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -6,11 +8,14 @@ from fastapir.config import Settings
 
 SQLALCHEMY_DATABASE_URL = Settings().database_url
 
+if os.getenv("GAE_APPLICATION", None):
+    connect_args = None
+else:
+    connect_args = {"check_same_thread": False}
+
 Base = declarative_base()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
