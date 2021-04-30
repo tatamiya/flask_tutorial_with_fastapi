a48c120d56e10bd639d36595b142d559bef07a72
tatamiya <tamyuji1002@gmail.com>
2021-04-30 12:25:35 +0900

    change: set string length limits for MySQL

--- fastapir/db/models.py
+++ fastapir/db/models.py
@@ -8,16 +8,16 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    username = Column(String(32), unique=True, index=True)
+    hashed_password = Column(String(256))
 
 
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    body = Column(String)
+    title = Column(String(64), index=True)
+    body = Column(String(1024))
     created_at = Column(Date)
     author_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", lazy="select")
